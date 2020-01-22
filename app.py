d14ea70175e635d7bd13d136b1eb33474317ad11
David Fenko <dfenko@fenkostan.com>
2020-01-22 10:46:44 -0600

    Advanced Pitcher Leaderboard - Partial

--- app.py
+++ app.py
@@ -41,6 +41,35 @@ class Pitches(Resource):
         json_response = json.loads(daily_schedule.to_json(orient='records', date_format = 'iso'))
         return(json_response)
 
+class AdvancedPitcher(Resource):
+    def get(self, start_date, end_date):
+        pl_host = os.getenv('PL_DB_HOST')
+        pl_db = 'pitcher-list'
+        pl_user = os.getenv('PL_DB_USER')
+        pl_password = os.getenv('PL_DB_PW')
+        db_connection = psycopg2.connect(host=pl_host, port=5432, dbname=pl_db, user=pl_user, password=pl_password)
+        cursor = db_connection.cursor()
+        cursor.execute("select pitchermlbamid, pitchername, count(*), avg(velo), \
+                        sum(case pitchresult when 'Foul' then 1 \
+                        when 'Hard Foul' then 1 else 0 end), \
+                        sum(case pitchresult when 'Swing Miss' then 1 \
+                        when 'Called Strike' then 1 when 'Foul' then 1 \
+                        when 'Hard Foul' then 1 when '-' then 1	else 0 end) \
+                        from pitches where ghuid in ( select ghuid \
+                        from schedule where game_date >= %s \
+                        and game_date <= %s) \
+                        group by pitchermlbamid, pitchername",
+                        [start_date, end_date])
+        rows = cursor.fetchall()
+        colnames = ['pitchermlbamid', 'pitchername', 'num_pitches',
+        'avg_velocity', 'num_foul', 'num_plus']
+        adv_pt = pd.DataFrame(rows, columns = colnames)
+        db_connection.close()
+        adv_pt['foul_pct'] = adv_pt.apply(lambda row: 100 * (int(row['num_foul']) / int(row['num_pitches'])), axis = 1)
+        adv_pt['plus_pct'] = adv_pt.apply(lambda row: 100 * (int(row['num_plus']) / int(row['num_pitches'])), axis = 1)
+        json_response = json.loads(adv_pt.to_json(orient='records', date_format = 'iso'))
+        return(json_response)
+
 class AdvancedPitchType(Resource):
     def get(self, start_date, end_date):
         pl_host = os.getenv('PL_DB_HOST')
@@ -50,7 +79,7 @@ class AdvancedPitchType(Resource):
         db_connection = psycopg2.connect(host=pl_host, port=5432, dbname=pl_db, user=pl_user, password=pl_password)
         cursor = db_connection.cursor()
         cursor.execute("select pitchermlbamid, pitchername, pitchtype, \
-                        count(*), round(avg(velo), 3), \
+                        count(*), avg(velo), \
                         sum(case pitchresult when 'Foul' then 1 \
                         when 'Hard Foul' then 1 else 0 end), \
                         sum(case pitchresult when 'Swing Miss' then 1 \
@@ -66,8 +95,8 @@ class AdvancedPitchType(Resource):
         'num_pitches', 'avg_velocity', 'num_foul', 'num_plus']
         adv_pt = pd.DataFrame(rows, columns = colnames)
         db_connection.close()
-        adv_pt['foul_pct'] = adv_pt.apply(lambda row: round(100 * (int(row['num_foul']) / int(row['num_pitches'])), 2), axis = 1)
-        adv_pt['plus_pct'] = adv_pt.apply(lambda row: round(100 * (int(row['num_plus']) / int(row['num_pitches'])), 2), axis = 1)
+        adv_pt['foul_pct'] = adv_pt.apply(lambda row: 100 * (int(row['num_foul']) / int(row['num_pitches'])), axis = 1)
+        adv_pt['plus_pct'] = adv_pt.apply(lambda row: 100 * (int(row['num_plus']) / int(row['num_pitches'])), axis = 1)
         json_response = json.loads(adv_pt.to_json(orient='records', date_format = 'iso'))
         return(json_response)
 
@@ -77,7 +106,8 @@ class HelloWorld(Resource):
 
 api.add_resource(Schedule, '/v1/Schedule/<string:game_date>')
 api.add_resource(Pitches, '/v1/Pitches/start_date-<string:start_date>&end_date-<string:end_date>')
-api.add_resource(AdvancedPitchType, '/v1/Advanced/Pitches/start_date-<string:start_date>&end_date-<string:end_date>')
+api.add_resource(AdvancedPitcher, '/v1/Advanced/Pitcher/start_date-<string:start_date>&end_date-<string:end_date>')
+api.add_resource(AdvancedPitchType, '/v1/Advanced/Pitch/start_date-<string:start_date>&end_date-<string:end_date>')
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
