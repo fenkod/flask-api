c9d61601355ad2aafc96353a226d30cbe2d3b22b
David Fenko <dfenko@fenkostan.com>
2020-01-22 11:02:38 -0600

    Fixed Plus Pitches

--- app.py
+++ app.py
@@ -25,22 +25,6 @@ class Schedule(Resource):
         json_response = json.loads(daily_schedule.to_json(orient='records', date_format = 'iso'))
         return(json_response)
 
-class Pitches(Resource):
-    def get(self, start_date, end_date):
-        pl_host = os.getenv('PL_DB_HOST')
-        pl_db = 'pitcher-list'
-        pl_user = os.getenv('PL_DB_USER')
-        pl_password = os.getenv('PL_DB_PW')
-        db_connection = psycopg2.connect(host=pl_host, port=5432, dbname=pl_db, user=pl_user, password=pl_password)
-        cursor = db_connection.cursor()
-        cursor.execute("SELECT * from pitches p where p.ghuid in (select s.ghuid from schedule s where s.game_date >= %s and s.game_date <= %s)", [start_date, end_date])
-        rows = cursor.fetchall()
-        colnames = [desc[0] for desc in cursor.description]
-        daily_schedule = pd.DataFrame(rows, columns = colnames)
-        db_connection.close()
-        json_response = json.loads(daily_schedule.to_json(orient='records', date_format = 'iso'))
-        return(json_response)
-
 class AdvancedPitcher(Resource):
     def get(self, start_date, end_date):
         pl_host = os.getenv('PL_DB_HOST')
@@ -52,9 +36,11 @@ class AdvancedPitcher(Resource):
         cursor.execute("select pitchermlbamid, pitchername, count(*), avg(velo), \
                         sum(case pitchresult when 'Foul' then 1 \
                         when 'Hard Foul' then 1 else 0 end), \
-                        sum(case pitchresult when 'Swing Miss' then 1 \
-                        when 'Called Strike' then 1 when 'Foul' then 1 \
-                        when 'Hard Foul' then 1 when '-' then 1	else 0 end) \
+                        sum(case when pitchresult = 'Swing Miss' then 1 \
+                        when pitchresult = 'Called Strike' then 1 \
+                        when pitchresult = 'Foul' AND (strikes = 0 OR strikes = 1) then 1 \
+                        when pitchresult = 'Hard Foul' AND (strikes = 0 OR strikes = 1) then 1 \
+                        when pitchresult = '-' then 1 else 0 end) \
                         from pitches where ghuid in ( select ghuid \
                         from schedule where game_date >= %s \
                         and game_date <= %s) \
@@ -82,9 +68,11 @@ class AdvancedPitchType(Resource):
                         count(*), avg(velo), \
                         sum(case pitchresult when 'Foul' then 1 \
                         when 'Hard Foul' then 1 else 0 end), \
-                        sum(case pitchresult when 'Swing Miss' then 1 \
-                        when 'Called Strike' then 1 when 'Foul' then 1 \
-                        when 'Hard Foul' then 1 when '-' then 1	else 0 end) \
+                        sum(case when pitchresult = 'Swing Miss' then 1 \
+                        when pitchresult = 'Called Strike' then 1 \
+                        when pitchresult = 'Foul' AND (strikes = 0 OR strikes = 1) then 1 \
+                        when pitchresult = 'Hard Foul' AND (strikes = 0 OR strikes = 1) then 1 \
+                        when pitchresult = '-' then 1 else 0 end) \
                         from pitches where ghuid in ( select ghuid \
                         from schedule where game_date >= %s \
                         and game_date <= %s) \
@@ -100,15 +88,14 @@ class AdvancedPitchType(Resource):
         json_response = json.loads(adv_pt.to_json(orient='records', date_format = 'iso'))
         return(json_response)
 
-class HelloWorld(Resource):
+class Status(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'status': 'available'}
 
 api.add_resource(Schedule, '/v1/Schedule/<string:game_date>')
-api.add_resource(Pitches, '/v1/Pitches/start_date-<string:start_date>&end_date-<string:end_date>')
 api.add_resource(AdvancedPitcher, '/v1/Advanced/Pitcher/start_date-<string:start_date>&end_date-<string:end_date>')
 api.add_resource(AdvancedPitchType, '/v1/Advanced/Pitch/start_date-<string:start_date>&end_date-<string:end_date>')
-api.add_resource(HelloWorld, '/')
+api.add_resource(Status, '/')
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port='8080')
