f140cbbd4a4c1ec16cd1fe4f642a416622dbcdc4
David Fenko <dfenko@fenkostan.com>
2020-01-22 10:19:09 -0600

    Partial Pitch Advanced Leaderboard

--- app.py
+++ app.py
@@ -41,6 +41,36 @@ class Pitches(Resource):
         json_response = json.loads(daily_schedule.to_json(orient='records', date_format = 'iso'))
         return(json_response)
 
+class AdvancedPitchType(Resource):
+    def get(self, start_date, end_date):
+        pl_host = os.getenv('PL_DB_HOST')
+        pl_db = 'pitcher-list'
+        pl_user = os.getenv('PL_DB_USER')
+        pl_password = os.getenv('PL_DB_PW')
+        db_connection = psycopg2.connect(host=pl_host, port=5432, dbname=pl_db, user=pl_user, password=pl_password)
+        cursor = db_connection.cursor()
+        cursor.execute("select pitchermlbamid, pitchername, pitchtype, \
+                        count(*), round(avg(velo), 3), \
+                        sum(case pitchresult when 'Foul' then 1 \
+                        when 'Hard Foul' then 1 else 0 end), \
+                        sum(case pitchresult when 'Swing Miss' then 1 \
+                        when 'Called Strike' then 1 when 'Foul' then 1 \
+                        when 'Hard Foul' then 1 when '-' then 1	else 0 end) \
+                        from pitches where ghuid in ( select ghuid \
+                        from schedule where game_date >= %s \
+                        and game_date <= %s) \
+                        group by pitchermlbamid, pitchername, pitchtype",
+                        [start_date, end_date])
+        rows = cursor.fetchall()
+        colnames = ['pitchermlbamid', 'pitchername', 'pitchtype',
+        'num_pitches', 'avg_velocity', 'num_foul', 'num_plus']
+        adv_pt = pd.DataFrame(rows, columns = colnames)
+        db_connection.close()
+        adv_pt['foul_pct'] = adv_pt.apply(lambda row: round(100 * (row['num_foul'] / row['num_pitches']), 2))
+        adv_pt['plus_pct'] = adv_pt.apply(lambda row: round(100 * (row['num_plus'] / row['num_pitches']), 2))
+        json_response = json.loads(adv_pt.to_json(orient='records', date_format = 'iso'))
+        return(json_response)
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
