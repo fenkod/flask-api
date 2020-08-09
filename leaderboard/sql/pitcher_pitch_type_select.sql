d9039b40bfa2bc86e3844041ae56d11f5456506b
David Fenko <dfenko@fenkostan.com>
2020-08-08 21:36:52 -0500

    Starts need to be left outer joined and coalesced

--- leaderboard/sql/pitcher_pitch_type_select.sql
+++ leaderboard/sql/pitcher_pitch_type_select.sql
@@ -3,5 +3,5 @@ select pitchermlbamid                                            as player_id
      , pitcherteam                                               as player_team
      , pitcherteam_abb                                           as player_team_abb
      , pitchtype                                                 as pitchtype
-     , start.num_starts                                          as num_starts
+     , coalesce(start.num_starts, 0)                             as num_starts
      --, pldp.num_pitches                                          as total_num_pitches
--- leaderboard/sql/start_sub_query_select.sql
+++ leaderboard/sql/start_sub_query_select.sql
@@ -1,4 +1,4 @@
-    join (
+    left outer join (
         select pitchermlbamid                                    as player_id
              , sum(sum)                                          as num_starts
         from pl_leaderboard_starts
