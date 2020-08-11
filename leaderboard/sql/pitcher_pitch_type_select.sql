fa0e5664f59b4955dd8d3834550be20adf46473b
David Fenko <dfenko@opploans.com>
2020-08-11 13:17:20 -0500

    Remove total number of pitches from pitch tabs

--- leaderboard/sql/pitcher_pitch_type_select.sql
+++ leaderboard/sql/pitcher_pitch_type_select.sql
@@ -4,4 +4,3 @@ select pitchermlbamid                                            as player_id
      , pitcherteam_abb                                           as player_team_abb
      , pitchtype                                                 as pitchtype
      , coalesce(start.num_starts, 0)                             as num_starts
-     --, pldp.num_pitches                                          as total_num_pitches
