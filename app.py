f0f8782cfd05a3442b44d4481d9a16f828b5bb12
William Gladstone <will@jollypebble.com>
2020-12-12 14:23:47 -0500

    add new endpoints

--- app.py
+++ app.py
@@ -10,38 +10,9 @@ import redis
 
 application = Flask(__name__)
 api = Api(application)
-# cache = Cache(application, config = {'CACHE_TYPE': 'simple'})
 cache = Cache()
 
-# cache_servers = os.environ.get('MEMCACHIER_SERVERS')
-# if cache_servers == None:
-#     # Fall back to simple in memory cache (development)
-#     cache.init_app(application, config={'CACHE_TYPE': 'simple'})
-# else:
-#     cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-#     cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-#     cache.init_app(application,
-#         config={'CACHE_TYPE': 'saslmemcached',
-#                 'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-#                 'CACHE_MEMCACHED_USERNAME': cache_user,
-#                 'CACHE_MEMCACHED_PASSWORD': cache_pass,
-#                 'CACHE_OPTIONS': { 'behaviors': {
-#                     # Faster IO
-#                     'tcp_nodelay': False,
-#                     # Keep connection alive
-#                     'tcp_keepalive': True,
-#                     # Timeout for set/get requests
-#                     'connect_timeout': 2000, # ms
-#                     'send_timeout': 750 * 1000, # us
-#                     'receive_timeout': 750 * 1000, # us
-#                     '_poll_timeout': 2000, # ms
-#                     # Better failover
-#                     'ketama': True,
-#                     'remove_failed': 1,
-#                     'retry_timeout': 2,
-#                     'dead_timeout': 30}}})
-
-
+# Cache Init
 redis_url = os.environ.get('REDIS_URL') or ''
 cache_invalidate_hour = 10
 if redis_url == '':
@@ -71,7 +42,7 @@ else:
                            'retry_timeout': 2,
                            'dead_timeout': 30}}})
 
-
+# Endpoint Handlers
 class Schedule(Resource):
     @cache.cached(timeout=300)
     def get(self, game_date):
@@ -641,7 +612,7 @@ class Leaderboard_2_1(Resource):
         logging.debug("JSON Response {json_response}")
         return (json_response)
 
-
+# Test Endpoint Handlers
 class Status(Resource):
     def get(self):
         return {'status': 'available'}
