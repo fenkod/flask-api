fd2995949c4a2f36e163733669f50aafcf6ba797
Steve Paulo <stevepaulo@gmail.com>
2021-01-07 14:19:57 -0800

    need separate resources for each endpoint

--- app.py
+++ app.py
@@ -641,7 +641,40 @@ class Leaderboard_2_1(Resource):
         logging.debug("JSON Response {json_response}")
         return (json_response)
 
+class Players(Resource):
+    @cache.cached(timeout = cache_timeout(cache_invalidate_hour))
+    def get(self, player_id='NA', positions='false'):
+
+        result = player_collection(player_id, positions)
+
+        json_response = json.loads(result.to_json(orient='records', date_format='iso'))
+        print("JSON Response {json_response}")
+        logging.debug("JSON Response {json_response}")
+        return (json_response)
+
 class Player(Resource):
+    @cache.cached(timeout = cache_timeout(cache_invalidate_hour))
+    def get(self, player_id='NA', positions='false'):
+
+        result = player_collection(player_id, positions)
+
+        json_response = json.loads(result.to_json(orient='records', date_format='iso'))
+        print("JSON Response {json_response}")
+        logging.debug("JSON Response {json_response}")
+        return (json_response)
+
+class PlayerPositions(Resource):
+    @cache.cached(timeout = cache_timeout(cache_invalidate_hour))
+    def get(self, player_id='NA', positions='false'):
+
+        result = player_collection(player_id, positions)
+
+        json_response = json.loads(result.to_json(orient='records', date_format='iso'))
+        print("JSON Response {json_response}")
+        logging.debug("JSON Response {json_response}")
+        return (json_response)
+
+class PlayersPositions(Resource):
     @cache.cached(timeout = cache_timeout(cache_invalidate_hour))
     def get(self, player_id='NA', positions='false'):
 
@@ -675,10 +708,10 @@ class ClearCache(Resource):
 # v2 Endpoints
 api.add_resource(Leaderboard_2, '/v2/leaderboard/leaderboard=<string:leaderboard>&handedness=<string:handedness>&opponent_handedness=<string:opponent_handedness>&league=<string:league>&division=<string:division>&team=<string:team>&home_away=<string:home_away>&year=<string:year>&month=<string:month>&half=<string:half>&arbitrary_start=<string:arbitrary_start>&arbitrary_end=<string:arbitrary_end>')
 api.add_resource(Leaderboard_2_1, '/v2_1/leaderboard/leaderboard=<string:leaderboard>&tab=<string:tab>&handedness=<string:handedness>&opponent_handedness=<string:opponent_handedness>&league=<string:league>&division=<string:division>&team=<string:team>&home_away=<string:home_away>&year=<string:year>&month=<string:month>&half=<string:half>&arbitrary_start=<string:arbitrary_start>&arbitrary_end=<string:arbitrary_end>')
-api.add_resource(Player, '/v2_1/players')
+api.add_resource(Players, '/v2_1/players')
 api.add_resource(Player, '/v2_1/players/player=<string:player_id>')
-api.add_resource(Player, '/v2_1/players/player=<string:player_id>&positions=<string:positions>')
-api.add_resource(Player, '/v2_1/players/positions=<string:positions>')
+api.add_resource(PlayersPositions, '/v2_1/players/positions=<string:positions>')
+api.add_resource(PlayerPositions, '/v2_1/players/player=<string:player_id>&positions=<string:positions>')
 
 # v1 Leaderboard Endpoints
 api.add_resource(AdvancedPitcher, '/v1/Advanced/Pitcher/start_date=<string:start_date>&end_date=<string:end_date>&year=<string:year>&month=<string:month>&half=<string:half>')
